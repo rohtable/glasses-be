d83810b7873fd3720999bf75d0426ab1d4c240d7
rohtable <96240967+rohtable@users.noreply.github.com>
2024-03-10 16:49:28 +0900

    default

---
+++ src/main/java/com/frenz4u/app/glassesbe/GlassesBeApplication.java
@@ -0,0 +1,13 @@
+package com.frenz4u.app.glassesbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlassesBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlassesBeApplication.class, args);
+	}
+
+}
